0e543bfc592f2ea5efb1fc3bb46f186d15d8a1a1
JanStreffing <jan.streffing@awi.de>
2021-02-08 19:16:59 +0100

    adding css

--- source/conf.py
+++ source/conf.py
@@ -185,3 +185,6 @@ epub_exclude_files = ['search.html']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+html_style = 'css/style.css'     # Choosing my custom theme.
+
